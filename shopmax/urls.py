f2bb0fabffe81430387611d5ef4002b0b146e513
Mariia Karabyn <maria0karabyn@gmail.com>
2024-02-27 13:20:19 +0100

    path

--- shopmax/urls.py
+++ shopmax/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import index
+from .views import index, regular, catalog, single_blog, single_product_details, blog, checkout, contact
 
 urlpatterns = [
-    path('', index, name='index' )
+    path('', index, name='index' ),
+    path('regular/', regular, name='regular' ),
+    path('catalog/', catalog, name='catalog' ),
+    path('single_blog/', single_blog, name='single_blog' ),
+    path('single_product_details/', single_product_details, name='single_product_details' ),
+    path('blog/', blog, name='blog' ),
+    path('checkout/', checkout, name='checkout' ),
+    path('contact/', contact, name='contact' ),
+    
 ]
--- shopmax/views.py
+++ shopmax/views.py
@@ -3,4 +3,23 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def regular(request):
+    return render(request, 'regular-page.html')
 
+def catalog(request):
+    return render(request, 'shop.html')
+
+def single_blog(request):
+    return render(request, 'single-blog.html')
+
+def single_product_details(request):
+    return render(request, 'single_product_details.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def contact(request):
+    return render(request, 'contact.html')
