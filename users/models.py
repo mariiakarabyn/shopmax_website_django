9110319f88e073f61758da2430dec3c7aab84cda
Mariia Karabyn <maria0karabyn@gmail.com>
2024-02-27 10:33:52 +0100

    managers

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=100, unique=True)
+    is_active = models.BooleanField(verbose_name='Active', default=True)
+    is_staff = models.BooleanField(verbose_name='Staff', default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
