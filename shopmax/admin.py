a9bfe760259f12c7205e2b53f5ecdc9f9ff98e6c
Mariia Karabyn <maria0karabyn@gmail.com>
2024-02-29 14:42:55 +0100

    Add models to the Django ShopMax admin

--- shopmax/admin.py
+++ shopmax/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from shopmax.models import Category, Product, Brand, Color, Size, Image
+
+admin.site.site_header = 'Django ShopMax'
+admin.site.site_title = 'Django ShopMax'
+admin.site.index_title = 'Django ShopMax'
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Color)
+admin.site.register(Size)
+admin.site.register(Image)
+admin.site.register(Product)
+
