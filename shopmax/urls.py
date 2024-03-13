3c96ae5e8adc9d3d6dce82f33df50df43f5a70c5
Mariia Karabyn <maria0karabyn@gmail.com>
2024-03-13 14:16:19 +0100

    top catagory area start, links in header

--- shopmax/urls.py
+++ shopmax/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import index, RegularViev, shop, SigdleBlogViev, product, BlogViev, CheckoutViev, ContactViev, AllProductsViev
+from .views import IndexView, RegularViev, ShopView, SigdleBlogViev, ProductView, BlogViev, CheckoutViev, ContactViev, AllProductsViev
 
 urlpatterns = [
-    path('', index, name='index' ),
+    path('', IndexView.as_view(), name='index' ),
     path('regular/', RegularViev.as_view(), name='regular' ),
     path('blog/', BlogViev.as_view(), name='blog' ),
     path('single_blog/', SigdleBlogViev.as_view(), name='single_blog' ),
     path('checkout/', CheckoutViev.as_view(), name='checkout' ),
     path('contact/', ContactViev.as_view(), name='contact' ),
-    path('shop/<slug:slug>/', shop, name='shop' ),
+    path('shop/<slug:slug>/', ShopView.as_view(), name='shop' ),
     path('product/', AllProductsViev.as_view(), name='all_products' ),
-    path('product/<slug:slug>/', product, name='product' ),
+    path('product/<slug:slug>/', ProductView.as_view(), name='product' ),
     
     
 ]
--- shopmax/views.py
+++ shopmax/views.py
@@ -33,46 +33,62 @@ class AllProductsViev(ListView):
         Prefetch('images', 
                  queryset=Image.objects.order_by('-size'))
         )
+        
+class ProductView(TemplateView):
+    template_name = 'product.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        single_product = get_object_or_404(Product, slug=self.kwargs.get('slug'))
+        main_images = Image.objects.filter(product=single_product).order_by('-size')[:4]
+        context['product'] = single_product
+        context['main_images'] = main_images
+        return context
 
-def index(request, slug=None):
-    if slug:
-        category = get_object_or_404(Category, slug=slug)
-        products = Product.objects.filter(categories=category).prefetch_related(
+class ShopView(ListView):
+    template_name = 'shop.html'
+    context_object_name = 'products'
+    paginate_by = 9
+
+    def get_queryset(self):
+        category_slug = self.kwargs.get('slug')
+        category = get_object_or_404(Category, slug=category_slug)
+        return Product.objects.filter(categories=category).prefetch_related(
             Prefetch('images', queryset=Image.objects.order_by('-size'))
-        )[:9]
-    else:
-        products = Product.objects.all().prefetch_related(
-            Prefetch('images', queryset=Image.objects.order_by('-size'))
-        )[:5]
+        )
 
-    brands = Brand.objects.all()[3:9]
-    context = {
-        'brands': brands,
-        'products': products
-    }
-    return render(request, 'index.html', context=context)
-
-
-def shop(request, **kwargs):
-    category = get_object_or_404(Category, slug=kwargs.get('slug'))
-    products = Product.objects.prefetch_related(
-        Prefetch('images', queryset=Image.objects.order_by('-size'))
-    ).filter(categories=category)[:9]
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_slug = self.kwargs.get('slug')
+        category = get_object_or_404(Category, slug=category_slug)
+        context['category_name'] = category.name
+        return context
     
-    context = {
-        'products': products
-    }
-    return render(request, 'shop.html', context)
 
+class IndexView(TemplateView):
+    template_name = 'index.html'
 
-def product(request, **kwargs):
-    single_product = get_object_or_404(Product, slug=kwargs.get('slug'))
-    main_images = Image.objects.filter(product=single_product).order_by('-size')[:4]
-    context= {
-        'product': single_product,
-        'main_images': main_images
-    }
-    return render(request, 'product.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_name = self.request.GET.get('category')
+        slug = self.kwargs.get('slug')
 
+        if category_name:
+            category = get_object_or_404(Category, name=category_name)
+            products = Product.objects.filter(categories=category).prefetch_related(
+                Prefetch('images', queryset=Image.objects.order_by('-size'))
+            )[:9]
+        elif slug:
+            category = get_object_or_404(Category, slug=slug)
+            products = Product.objects.filter(categories=category).prefetch_related(
+                Prefetch('images', queryset=Image.objects.order_by('-size'))
+            )[:9]
+        else:
+            products = Product.objects.all().prefetch_related(
+                Prefetch('images', queryset=Image.objects.order_by('-size'))
+            )[:5]
 
+        brands = Brand.objects.all()[3:9]
+        context['brands'] = brands
+        context['products'] = products
+        return context
